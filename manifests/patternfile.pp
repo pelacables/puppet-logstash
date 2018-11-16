04ce176faff1c693d1102b3dd833ba268aa20d0c
Arnau <arnau.bria@gmail.com>
2018-11-16 16:00:09 +0100

    Update patternfile.pp
    add teh possibility of adding content directly.

--- manifests/patternfile.pp
+++ manifests/patternfile.pp
@@ -1,38 +1,76 @@
-# This type represents a Grok pattern file for Logstash.
+# == Define: logstash::patternfile
 #
-# @param [String] source
-#   File source for the pattern file. eg. `puppet://[...]` or `file://[...]`
+# This define allows you to transport custom pattern files to the Logstash instance
 #
-# @param [String] filename
-#   Optionally set the destination filename.
+# All default values are defined in the logstashc::params class.
 #
-# @example Define a pattern file.
-#   logstash::patternfile { 'mypattern':
-#     source => 'puppet:///path/to/my/custom/pattern'
-#   }
 #
-# @example Define a pattern file with an explicit destination filename.
-#   logstash::patternfile { 'mypattern':
-#     source   => 'puppet:///path/to/my/custom/pattern',
-#     filename => 'custom-pattern-name'
-#   }
+# === Parameters
 #
-# @author https://github.com/elastic/puppet-logstash/graphs/contributors
+# [*source*]
+#   Puppet file resource of the pattern file ( puppet:// )
+#   Value type is string
+#   Default value: None
+#
+# [*content*]
+#   Content of the Pattern file
+#   Value type is string
+#   Default value: None
+#
+# [*filename*]
+#   if you would like the actual file name to be different then the source file name
+#   Value type is string
+#   This variable is optional
+#
+#
+# === Examples
+#
+#     logstash::patternfile { 'mypattern':
+#       source => 'puppet:///path/to/my/custom/pattern'
+#     }
+#
+#     or wil an other actual file name
+#
+#     logstash::patternfile { 'mypattern':
+#       source   => 'puppet:///path/to/my/custom/pattern_v1',
+#       filename => 'custom_pattern'
+#     }
+#
+#
+# === Authors
+#
+# * Justin Lambert
+# * Richard Pijnenburg <mailto:richard.pijnenburg@elasticsearch.com>
 #
 define logstash::patternfile (
-  Pattern[/^(puppet|file):\/\//] $source   = undef,
-  Optional[String[1]]            $filename = undef,
-) {
-  require logstash::config
+  $source = undef,
+  $content = undef,
+  $filename = '',
+){
 
-  $destination = pick($filename, basename($source))
+  if $source {
+    validate_re($source, '^(puppet|file)://', 'Source must be from a puppet fileserver or a locally accessible file (begins with either puppet:// or file://)' )
+    $filename_real = $filename ? {
+      ''      => inline_template('<%= @source.split("/").last %>'),
+      default => $filename
+    }
+  }
+  elsif $content {
+    $filename_real = $title
+  }
+  else {
+    fail("you must specify ${source} or ${content}")
+  }
 
-  file { "${logstash::config_dir}/patterns/${destination}":
-    ensure => file,
-    source => $source,
-    owner  => 'root',
-    group  => $logstash::logstash_group,
-    mode   => '0640',
-    tag    => ['logstash_config'],
+  $patterns_dir = "${logstash::configdir}/patterns"
+
+
+  file { "${patterns_dir}/${filename_real}":
+    ensure  => 'file',
+    owner   => $logstash::logstash_user,
+    group   => $logstash::logstash_group,
+    mode    => '0644',
+    source  => $source,
+    content => $content,
   }
 }
